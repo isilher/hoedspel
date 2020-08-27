27c1936015d4d56d60194ab6b0d101328385753a
Rik <ik@rikvanduijn.nl>
2020-08-27 13:52:40 +0200

    Updates

---
+++ precache-manifest.2708ce7c5763220e939c1ed3cf50fdeb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b83ec9146689c533983",
+    "url": "/hoedspel/static/js/app.3b830803.chunk.js"
+  },
+  {
+    "revision": "eb641bff62f782fa41c3",
+    "url": "/hoedspel/static/js/runtime~app.f2c68294.js"
+  },
+  {
+    "revision": "02c65b5436ebfd050b32",
+    "url": "/hoedspel/static/js/2.f482aaff.chunk.js"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/hoedspel/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/hoedspel/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/hoedspel/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/hoedspel/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/hoedspel/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hoedspel/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/hoedspel/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/hoedspel/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/hoedspel/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/hoedspel/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/hoedspel/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/hoedspel/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/hoedspel/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "a3b01432f43d69845ae2d82fd0494ddf",
+    "url": "/hoedspel/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/hoedspel/serve.json"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/hoedspel/expo-service-worker.js"
+  },
+  {
+    "revision": "699d6274b02f19a2541dd259d98cc8e2",
+    "url": "/hoedspel/register-service-worker.js"
+  },
+  {
+    "revision": "e0f7f808b0e4815795fb184e7ab4e065",
+    "url": "/hoedspel/static/js/2.f482aaff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/hoedspel/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/hoedspel/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/hoedspel/favicon.ico"
+  },
+  {
+    "revision": "f6bd4d48a1f21b7162156d0f0f7b75cc",
+    "url": "/hoedspel/manifest.json"
+  }
+];
